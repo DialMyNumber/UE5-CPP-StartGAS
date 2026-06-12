25229ffab53beedf89fe9e9d9e446c090161540e
DialMyNumber <90255861+DialMyNumber@users.noreply.github.com>
2026-06-12 14:45:54 +0900

    GAS를 이용하는 CharacterBase 생성

---
+++ Source/MyBPGAS/Private/Character/GAS_CharacterBase.cpp
@@ -0,0 +1,35 @@
+﻿// GAS_CharacterBase.cpp
+
+#include "Character/GAS_CharacterBase.h"
+
+
+AGAS_CharacterBase::AGAS_CharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+}
+
+void AGAS_CharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGAS_CharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AGAS_CharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+// 현재 이 CharacterBase가 가지고 있는 AbilitySystemComponent를 반환하는 Getter
+UAbilitySystemComponent* AGAS_CharacterBase::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
---
+++ Source/MyBPGAS/Public/Character/GAS_CharacterBase.h
@@ -0,0 +1,38 @@
+﻿// GAS_CharacterBase.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"				// GAS
+#include "AbilitySystemComponent.h"				// GAS
+#include "GAS_CharacterBase.generated.h"
+
+UCLASS()
+class MYBPGAS_API AGAS_CharacterBase 
+	: public ACharacter
+	, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AGAS_CharacterBase();
+
+	// Get GAS Component
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+protected:
+	// 캐릭터 하나당 하나씩 붙는 AbilitySystemComponent
+	// HP, MP 같은 변수들이 이 컴포넌트에 붙음
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AbilitySystem")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
